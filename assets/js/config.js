daa7528d2e4de47eb1f787805a9be364a4e7805a
Bmiki6476 <Bmiki6476@users.noreply.github.com>
2026-09-23 14:03:25 +0200

    Rezervácie cez Google Tabuľku (automatické e-maily)

--- assets/js/config.js
+++ assets/js/config.js
@@ -2,7 +2,7 @@
 window.SF_CONFIG = {
   // Adresa Google Apps Script web aplikácie (postup v NAVOD.md, časť A).
   // Kým je prázdna, web beží v ukážkovom režime z data/akcie.json a data/novinky.json.
-  API_URL: '',
+  API_URL: 'https://script.google.com/macros/s/AKfycbyDsLbxlkW6W8Apq6QCfx36QYcPm5KqYcEC2OmhtpqC5iA0AIUddIj87dGg40tVzdbj1A/exec',
 
   // true = kým nie je API_URL, rezervácia sa odošle e-mailom na adresu nižšie (web funguje aj bez Googlu)
   REZERVACIA_MAILOM: true,
